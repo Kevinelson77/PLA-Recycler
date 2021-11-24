71320aaa23c7cb2d0511a4cc77b0f8c4eb8d1119
Kevin Nelson <34145860+Kevinelson77@users.noreply.github.com>
2021-11-24 14:58:08 -0500

    Add files via upload

---
+++ BluefruitConfig.h
@@ -0,0 +1,129 @@
+#define BUFSIZE                        128   // Size of the read buffer for incoming data
+#define VERBOSE_MODE                   true  // If set to 'true' enables debug output
+
+// SOFTWARE UART SETTINGS
+// The following macros declare the pins that will be used for 'SW' serial.
+#define BLUEFRUIT_SWUART_RXD_PIN       9
+#define BLUEFRUIT_SWUART_TXD_PIN       10
+#define BLUEFRUIT_UART_CTS_PIN         11
+#define BLUEFRUIT_UART_RTS_PIN         8
+#define BLUEFRUIT_UART_MODE_PIN        -1 
+
+// FACTORYRESET_ENABLE       Perform a factory reset when running(1 will enable reset)
+#define FACTORYRESET_ENABLE         0
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
+
+// Pin definitions
+#define RPM       2
+#define MAXDO     3
+#define MAXCLK    4
+#define MAXCS1    5
+#define MAXCS2    6
+#define DoubleBandPowerPin 7
+#define SingleBandPowerPin 8
+#define MotorPower A0
+
+////////// Initiallizing All Parameters //////////
+
+// Thermocouple
+Adafruit_MAX31855 thermocouple1(MAXCLK, MAXCS1, MAXDO);
+Adafruit_MAX31855 thermocouple2(MAXCLK, MAXCS2, MAXDO);
+
+//PID Controller
+//double Setpoint, Input, Output;
+//int WindowSize = 5000;
+//unsigned long windowStartTime;
+//PID myPID(&Input, &Output, &Setpoint, 2, 0, 0, DIRECT);
+//
+//// Stepper Motor
+//int StepsRequired;
+//const float steps_per_rev = 32;                             //Internal motor revolution
+//const float gear_reduction = 64;                            //Gear reduction
+//const float total_steps = steps_per_rev*gear_reduction;     //Total # of steps
+//Stepper steppermotor(steps_per_rev, 9, 11, 10, 12);         // Pins used are 9, 10, 11, 12(IN1, IN2, IN3, IN4, respectively)
+
+// RPM Counter
+//unsigned int counter = 0;
+//float diskslots = 24.00;
+//float rotation;
+
+//// Timer Count
+//void ISR_count(){
+//  counter++;
+//}
+
+//// Timer
+//void ISR_timerone(){
+//  Timer1.detachInterrupt();
+//  float rotation = (counter / diskslots) * 60.00;
+//  counter = 0;
+//  Timer1.attachInterrupt(ISR_timerone);
+//}
+
+// A small helper
+//void error(const __FlashStringHelper*err) {
+//  Serial.println(err);
+//  while (1);
+//}
+
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN, BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+
+void setup(void){
+  // Pin Setup, Bluetooth uses A0->A3
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, HIGH);
+ // pinMode(A1, OUTPUT);
+ // digitalWrite(A1, HIGH);
+//  pinMode(A2, OUTPUT);
+//  digitalWrite(A2, HIGH);
+//  pinMode(A3, OUTPUT);
+//  digitalWrite(A3, HIGH);
+  pinMode(DoubleBandPowerPin, OUTPUT);
+  pinMode(SingleBandPowerPin, OUTPUT);
+
+  // PID Setup
+//  windowStartTime = millis();
+//  Setpoint = 180;
+//  myPID.SetOutputLimits(0, WindowSize);
+//  myPID.SetMode(AUTOMATIC);
+//
+//  while (!Serial);
+//  delay(500);
+
+  ///// Turn on heaters for 8 seconds ////
+  Serial.begin(9600);
+  digitalWrite(DoubleBandPowerPin, HIGH);
+  digitalWrite(SingleBandPowerPin, HIGH);
+  delay(8000);
+
+  // Initialize timer interrupt
+//  Timer1.initialize(1000000);
+//  attachInterrupt(digitalPinToInterrupt(2), ISR_count, RISING);
+//  Timer1.attachInterrupt(ISR_timerone);
+
+  // Begin Bluetooth
+//  Serial.begin(115200);
+//  if ( !ble.begin(VERBOSE_MODE) ){
+//    error(F("Couldn't find Bluefruit, make sure it's in Command mode & check wiring?"));
+//  }
+//  Serial.println( F("OK!") );
+//  if (FACTORYRESET_ENABLE){
+//    Serial.println(F("Performing a factory reset: "));
+//    if ( ! ble.factoryReset() ){
+//      error(F("Couldn't factory reset"));
+//    }
+//  }
+//  
+//  ble.echo(false);
+//  ble.verbose(false);  // debug info is a little annoying after this point!
+//
+//  while (! ble.isConnected()){
+//    delay(500);}
+//
+//  // LED Activity command is only supported from 0.6.6
+//  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION)){
+//    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR); // Change Mode LED Activity
+//  }
+}
---
+++ code_revision3.ino
@@ -0,0 +1,93 @@
+#include <SPI.h>
+#include <Stepper.h>
+#include <SoftwareSerial.h>
+#include <Arduino.h>
+#include <PID_v1.h>
+#include "TimerOne.h"
+#include "Adafruit_MAX31855.h"
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_UART.h"
+#include "BluefruitConfig.h"
+
+void loop() {
+
+   double DoubleBand = thermocouple1.readCelsius();
+   if (isnan(DoubleBand)) {
+     Serial.println("Something wrong with thermocouple!");
+   } else {
+     Serial.print("Double Band = ");
+     Serial.println(DoubleBand);
+   }
+
+   double SingleBand = thermocouple2.readCelsius();
+   if (isnan(SingleBand)) {
+     Serial.println("Something wrong with thermocouple!");
+   } else {
+     Serial.print("Single Band = ");
+     Serial.println(SingleBand);
+   }
+
+   if(DoubleBand = 0.0){
+     digitalWrite(DoubleBandPowerPin, LOW);}
+   else if(DoubleBand <= 160.0) {
+    digitalWrite(DoubleBandPowerPin, HIGH);} 
+   else if(DoubleBand > 160.0){
+     digitalWrite(DoubleBandPowerPin, LOW);}
+   else {
+     digitalWrite(DoubleBandPowerPin, LOW);}
+   if(SingleBand = 0.0){
+     digitalWrite(SingleBandPowerPin, LOW);}
+   else if(SingleBand <= 170.0) {
+    digitalWrite(SingleBandPowerPin, HIGH);} 
+   else if(SingleBand > 170.0){
+     digitalWrite(SingleBandPowerPin, LOW);}
+   else {
+     digitalWrite(SingleBandPowerPin, LOW);}
+
+   if((DoubleBandPowerPin && SingleBandPowerPin) == HIGH){
+     digitalWrite(MotorPower, HIGH);
+   }
+   else { digitalWrite(MotorPower, LOW);}
+
+
+}
+     
+  ///// Read all sensors /////
+//  String temp1write = "temp1";
+//  String temp1 = temp1write + thermocouple1.readCelsius();
+//  ble.print("AT+BLEUARTTX=");
+//  ble.println(temp1);
+//  String temp2write = "temp2";
+//  String temp2 = temp2write + thermocouple2.readCelsius();
+//  ble.print("AT+BLEUARTTX=");
+//  ble.println(temp2);
+//  String AugurRPMWrite = "auguerSpeed";
+//  String AugurRPM = AugurRPMWrite + rotation;
+//  ble.print("AT+BLEUARTTX=");
+//  ble.println(AugurRPM);
+//  String diameterWrite = "diameter";
+//  String diameter = diameterWrite + diameter;
+//  ble.print("AT+BLEUARTTX=");
+//  ble.println(diameter);
+//  String pumpSpeedWrite = "pumpSpeed";
+//  String pumpSpeed = pumpSpeedWrite + pumpSpeed;
+//  ble.print("AT+BLEUARTTX=");
+//  ble.println(pumpSpeed);
+
+
+  ///// Output temperature based off reading /////
+//  double input1 = thermocouple1.readCelsius();
+//  double input2 = thermocouple2.readCelsius();
+//  myPID.Compute();
+//  unsigned long now = millis();
+//  if (now - windowStartTime > WindowSize){
+//    windowStartTime += WindowSize; }
+//  if (Output > now - windowStartTime){
+//    digitalWrite(PowerPin1 && PowerPin2, HIGH); }
+//  else {digitalWrite(PowerPin1 && PowerPin2, LOW); }
+   
+  ///// Set puller motor speed /////
+//  StepsRequired = total_steps;
+//  steppermotor.setSpeed(350);
+//  steppermotor.step(StepsRequired);
+//}
